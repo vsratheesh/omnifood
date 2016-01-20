7b411f8c2248236516b696affaac32417bbc1afb
vsratheesh <vsratheesh95@gmail.com>
2016-01-20 12:32:38 +0530

    added smooth scroll

--- Reources/js/script.js
+++ Reources/js/script.js
@@ -15,11 +15,11 @@ $(document).ready(function()   {
   /* scroll   on buttons */
     
   $('.js-scroll-to-plans').click(function(){
-    $('html,body').animate({scrolltop:$('.js-section-plans').offset().top},1000);
+    $('html,body').animate({scrollTop: $('.js-section-plans').offset().top},1000);
   });
     
     $('.js-scroll-to-start').click(function(){
-    $('html,body').animate({scrolltop:$('.js-section-features').offset().top},1000);
+    $('html,body').animate({scrollTop: $('.js-section-features').offset().top},1000);
   });
 });
 
